e8953f2d715d1c3f2eea5c8ea13139bb4375f5da
Benkolon152 <v.benasz@gmail.com>
2025-10-02 12:12:17 +0200

    Create project

---
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -0,0 +1,24 @@
+package com.example.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+    public TextField firstTextField;
+    public ChoiceBox operationChoiceBox;
+    public TextField secondTextField;
+    public Label resultLabel;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX CalculatorApplication!");
+    }
+
+    public void handleCalculate(ActionEvent actionEvent) {
+    }
+}
